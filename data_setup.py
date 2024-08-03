6c8883ba9e0e61b2fbbf2b799b084c35b166ef0b
williamyeh <wy16@illinois.edu>
2024-08-02 19:16:59 -0500

    downloaded data

---
+++ data_setup.py
@@ -0,0 +1,6 @@
+from helper_functions import *
+# Download pizza, steak, sushi images from GitHub
+data_20_percent_path = download_data(source="https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi_20_percent.zip",
+                                     destination="pizza_steak_sushi_20_percent")
+
+data_20_percent_path
